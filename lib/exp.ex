b59516dd5222675ee171989fb3e159496e6e4f74
Eike Lurz <eike.lurz@tuta.io>
2024-11-11 13:02:50 +0100

    Add some combinators

--- lib/exp.ex
+++ lib/exp.ex
@@ -16,6 +16,8 @@ defmodule Exp do
     :world
   end
 
+  # Allowed infix operators
+  # \\, <-, |, ~>>, <<~, ~>, <~, <~>, <|>, <<<, >>>, |||, &&&, and ^^^.
   defmodule Parser do
     def item() do
       fn
@@ -35,8 +37,41 @@ defmodule Exp do
       fn cs -> p.(cs) |> Enum.flat_map(fn {c, cs} -> [{f.(c), cs}] end) end
     end
 
+    def p ~>> f do
+      bind(p, f)
+    end
+
     def map(p, f) do
       fn cs -> p.(cs) |> Enum.map(fn {c, cs} -> {f.(c), cs} end) end
+    end
+
+    def p >>> f do
+      map(p, f)
+    end
+
+    def zero() do
+      fn _ -> [] end
+    end
+
+    def and_(p, q) do
+      fn cs -> p.(cs) ++ q.(cs) end
+    end
+
+    def p <~> q do
+      and_(p, q)
+    end
+
+    def or_(p, q) do
+      fn cs ->
+        case and_(p, q).(cs) do
+          [] -> []
+          [x | _] -> [x]
+        end
+      end
+    end
+
+    def p <|> q do
+      or_(p, q)
     end
   end
 end
